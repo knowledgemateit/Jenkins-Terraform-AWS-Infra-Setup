10e90ea3351a0eee04f4bdd263015d16b09003c2
knowledgemateit <103619729+knowledgemateit@users.noreply.github.com>
2026-02-28 08:17:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.0"
     }
   }
 }
@@ -60,6 +60,7 @@ resource "aws_instance" "ec2_private" {
 }
 ################################
  
+
 
 
 
